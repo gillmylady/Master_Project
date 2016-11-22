1ee32ae846e142062b18a43c2c99208fb1af3b7b
gillmylady <myladywh@hotmail.com>
2016-11-22 01:45:21 -0500

    Merge origin/master

--- src/master/project/main/MasterProject.java
+++ src/master/project/main/MasterProject.java
@@ -96,7 +96,7 @@ public class MasterProject {
         
         //RunEachInstanceWithDifferentOption(-1);
         
-        RunAllInstancesInSameCaseWithLimitedTime(true, 13, true, true, true, true, true, true, false, true);
+        RunAllInstancesInSameCaseWithLimitedTime(true, 13, true, true, true, true, true, true, true, true);
         
         //compareConstructiveSolution();
         
